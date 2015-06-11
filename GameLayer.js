ef3e180f8594934ff90a1a8a34804ed84f83b52e
weaklai <luo_ji@qq.com>
2015-06-11 19:14:26 +0800

    new

--- GameLayer.js
+++ GameLayer.js
@@ -12,7 +12,7 @@ STATE_GAMEOVER = 1;
 MAX_CONTAINT_WIDTH = 40;
 MAX_CONTAINT_HEIGHT = 40;
 
-var g_sharedGameLyaer;
+var g_sharedGameLayer;
 
 var GameLayer = cc.Layer.extend({
     _time:null,
@@ -35,11 +35,205 @@ var GameLayer = cc.Layer.extend({
 
     ctor:function()
     {
+        this._super();
+        this.init();
+    },
+    init:function ()
+    {
         cc.spriteFrameCache.addSpriteFrames(res.textureOpaquePack_plist);
         cc.spriteFrameCache.addSpriteFrames(res.b01_plist);
 
+        MW.CONTAINER.ENEMIES = [];
+        MW.CONTAINER.ENEMY_BLLETS = [];
+        MW.CONTAINER.PLAYER_BULLETS = [];
+        MW.CONTAINER.EXPLOSIONS = [];
+        MW.CONTAINER.SPARKS = [];
+        MW.CONTAINER.HITS = [];
+        MW.CONTAINER.BACKSKYS = [];
+        MW.CONTAINER.BACKTILEMAPS = [];
+        MW.ACTIVE_ENEMIES = 0;
 
+        MW.SCORE = 0;
+        MW.LIFE = 4;
+        this._state = STATE_PLAYING;
+
+        var texOpaque = cc.textureCache.addImage(res.textureOpaquePack_png);
+        this._texOpaqueBatch = new cc.SpriteBatchNode(texOpaque);
+        this._sparkBatch = new cc.SpriteBatchNode(texOpaque);
+        if (cc.sys.isNative)
+        {
+            this._sparkBatch.setBlendFunc(cc.SRC_ALPHA, cc.ONE);
+        }
+        this.addChild(this._texOpaqueBatch);
+        this.addChild(this._sparkBatch);
+
+        var texTransparent = cc.textureCache.addImage(res.textureTransparentPack_png);
+        this._texTranSparentBatch = new cc.SpriteBatchNode(texTransparent);
+        this.addChild(this._texTransparentBatch);
+
+        winSize = cc.director.getWinSize();
+        this._levelManager = new LevelManager(this);
+
+        this.screenRect = cc.rect(0, 0, winSize.width, winSize.height + 10);
+
+        this.lbScore = new cc.LabelBMFont("Score: 0", res.arial_14_fnt);
+        this.lbScore.attr({
+            anchorX: 1,
+            anchorY: 0,
+            x: winSize.width - 5,
+            y: winSize.height - 30,
+            scale: MW.SCALE
+        });
+        this.lbScore.textAlign = cc.TEXT_ALIGNMENT_RIGHT;
+        this.addChild(this,lbScore, 1000);
+
+        var life = new cc.Sprite("#ship03.png");
+        life.attr({
+            scale: 0.6,
+            x: 30,
+            y: MW.HEIGHT - 30
+        });
+        this._textTransparentBatch.addChild(life, 1, 5);
+
+        this._lbLife = new cc.LabelTTF("0", "Arial", 20);
+        this._lbLife.x = 60;
+        this._lbLife.y = MW.HEIGHT - 25;
+        this._lbLife.color = cc.color(255, 0, 0);
+        this.addChild(this._lbLife, 1000);
+
+        this._ship = new Ship();
+        this._texTransparentBatch.addChild(this._ship, this._ship.zOrder, MW.UNIT_TAG.PALYER);
+
+        cc.spriteFrameCache.addSpriteFrames(res.explosion_plist);
+        var explosionTexture = cc.textureCache.addImage(rex.explosion_png);
+        this._explosions = new cc.SpriteBatchNode(explosionTexture);
+        this._explosions.setBlendFunc(cc.SRC_ALPHA, cc.ONE);
+        this.addChild(this._explosions);
+        Explosion.sharedExplosion();
+
+        if (cc.sys.capabilities.hasOwnProperty('touches'))
+        {
+            cc.eventManager.addListener({
+                prevTouchID: -1,
+                event: cc.EventListener.TOUCH_ALL_AT_ONCE,
+                onTouchesMoved:function (touches, event)
+                {
+                    var touch = touches[0];
+                    if (this.prevTouchId != touch.getID())
+                    {
+                        this.prevTouchId = touch.getID();
+                    }
+                    else
+                    {
+                        event.getCurrentTarget().processEvent(touches[0]);
+                    }
+                }
+            }, this);
+        }
+
+        this.scheduleUpdate();
+        this.schedule(this.scoreCounter, 1);
+
+        if (MW.SOUND)
+        {
+            cc.audioEngine.playMusic(cc.sys.os == cc.sys.OS_WP8 || cc.sys.os == cc.sys.OS_WINRT ? res.bgMusic_wav : res.bgMusic_mp3, true);
+        }
+
+        g_sharedGameLayer = this;
+
+        Bullet.preSet();
+        Enemy.preSet();
+        HitEffect.preSet();
+        SparkEffect.preSet();
+        Explosion.preSet();
+        BackSky.preSet();
+        BackTileMap.preSet();
+
+        this.initBackground();
+
+        return true;
     },
 
+    scoreCounter:function()
+    {
+        if (this._state == STATE_PALEYING)
+        {
+            this._time++;
+            this._levelManager.loadLevelResource(this._time);
+        }
+    },
 
+    processEvent:function (event)
+    {
+        if (this._state == STATE_PLAYING)
+        {
+            var delta = event.getDelta();
+            var curPos = cc.p(this._ship.x, this._ship.y);
+            curPos = cc.pAdd(curPos, delta);
+            curPos = cc.pClamp(curPos, cc.p(0, 0), cc.p(winSize.width, winSize.height));
+            this._ship.x = curPos.x;
+            this._ship.y = curPos.y;
+            curPos = null;
+        }
+    },
+
+    update:function(dt)
+    {
+        if (this._state == STATE_PLAYING)
+        {
+            this.checkIsCollide();
+            this.removeInactiveUnit(dt);
+            this.checkIsReborn();
+            this.updateUI();
+            this._movingBackground(dt);
+        }
+    },
+    checkIsCollide:function()
+    {
+        var selChild, bulletChild;
+
+        var i, locShip = this._ship;
+        for (i=0; i<MW.CONTAINER.ENEMIES.length; ++i)
+        {
+            selChild = MW.CONTAINER.ENEMIES[i];
+            if (!selChild.active)
+            {
+                continue;
+            }
+            for (var j=0; j<MW.CONTAINER.PLAYER_BULLETS.length; ++j)
+            {
+                bulletChild = MW.CONTEINER.PLAYER_BULLETS[j];
+                if (bulletChild.active && this.collide(selChild, bulletChild))
+                {
+                    bulletChild.hurt();
+                    selChild.hurt();
+                }
+            }
+            if (this.collide(selChild, locShip))
+            {
+                if (locShip.active)
+                {
+                    selChild.hurt();
+                    locShip.hurt();
+                }
+            }
+        }
+
+        for (i=0; i<MW.CONTAINER.ENEMY_BULLETS.length; ++i)
+        {
+            selChild = MW.CONTAINER.ENEMY_BULLETS[i];
+            if (selChild.active && this.collide(selChild, locShip))
+            {
+                if (locShip.active)
+                {
+                    selChild.hurt();
+                    locShip.hurt();
+                }
+            }
+        }
+    },
+    removeInactiveUnit:function (dt)
+    {
+
+    }
 });
